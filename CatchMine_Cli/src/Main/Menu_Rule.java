09a1e70f8a7f12a4bf956806e688aa3c9e0bc29a
cjy1521 <cjy1521@naver.com>
2019-10-31 09:24:03 +0900

    게임 룰 이미지추가예정 주석

--- CatchMine_Cli/src/Main/Menu_Rule.java
+++ CatchMine_Cli/src/Main/Menu_Rule.java
@@ -1,14 +1,19 @@
 package Main;
+
+import java.awt.Image;
+import java.awt.Toolkit;
+
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
-public class Menu_Rule extends JPanel{
-
-	Design de = new Design();
+public class Menu_Rule extends JPanel {
 	
+	Design de = new Design();
+
 	public Menu_Rule() {
 		setSize(400, 600);
 		setLayout(null);
-		
+
 		add(de.setBackground());
 	}
 }
