38631b02fe666f6afd51ab3a9a46ca82be6b94e0
Pum0 <daehwan9960@naver.com>
2019-12-08 05:52:47 +0900

    클라이어트, 서버 틀

---
+++ CatchMine_Ser/src/ServerThread.java
@@ -0,0 +1,23 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerThread implements Runnable {
+
+	ServerSocket serverSocket;
+	Socket client;
+
+	public ServerThread(ServerSocket serverSocket) {
+		this.serverSocket = serverSocket;
+	}
+
+	public void run() {
+		while (true) {
+			try {
+			client = serverSocket.accept();
+			System.out.println("connected client : " + client);
+			} catch(Exception e) {
+				
+			}
+		}
+	}
+}
