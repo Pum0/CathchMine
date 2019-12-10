52d4d46ea7165f585e737cf4e800448e390be5d9
cjy1521 <cjy1521@naver.com>
2019-12-10 13:26:48 +0900

    게임 룰 수정 , 이미지 대폭 수정

--- CatchMine_Cli/src/Scenes/Menu_Rule.java
+++ CatchMine_Cli/src/Scenes/Menu_Rule.java
@@ -16,11 +16,13 @@ import MyListener.Mouse;
 
 public class Menu_Rule extends JPanel {
 	JPanel mainPanel;
-	JLabel backMoveLabel, leftMoveLabel, rightMoveLabel, frontMoveLabel, keyLabel;
-	ImageIcon backMoveIcon, leftMoveIcon, rightMoveIcon, frontMoveIcon, keyIcon;
+	JLabel backMoveLabel, leftMoveLabel, rightMoveLabel, frontMoveLabel, keyLabel, AKeyLabel, SKeyLabel, DKeyLabel,
+			JumpLabel, FlagLabel, QMLable, SKeyLable2, SKeyLable3, ItemLabel, ItemLabel2;
+	ImageIcon backMoveIcon, leftMoveIcon, rightMoveIcon, frontMoveIcon, keyIcon, AKeyIcon, SKeyIcon, DKeyIcon, JumpIcon,
+			FlagIcon, QMIcon, SKeyIcon2, SKeyIcon3, ItemIcon, ItemIcon2;
 
 	JScrollPane panelPane;
-	
+
 	public JButton backButton;
 
 	Action al = new Action();
@@ -39,41 +41,93 @@ public class Menu_Rule extends JPanel {
 		Dimension size = new Dimension();
 		size.setSize(280, 400);
 		mainPanel.setPreferredSize(size);
-		
+
 		panelPane = new JScrollPane(mainPanel);
 		panelPane.setBounds(40, 40, 300, 400);
-		
+
 		backButton = new JButton("뒤로가기", new ImageIcon("image/button/100x40.png"));
 		backButton.setHorizontalTextPosition(SwingConstants.CENTER);
 		backButton.setFont(de.setFont(14));
 		backButton.setBorderPainted(false);
 		backButton.setBounds(140, 460, 100, 40);
 
-		backMoveIcon = new ImageIcon("image/gif/Back_Move.gif");
+		backMoveIcon = new ImageIcon("image/Key_Image/Back_Move.gif");
 		backMoveLabel = new JLabel(backMoveIcon);
-		backMoveLabel.setBounds(60, 20, 40, 40);
+		backMoveLabel.setBounds(50, 20, 40, 40);
 
-		leftMoveIcon = new ImageIcon("image/gif/Left_Move.gif");
+		leftMoveIcon = new ImageIcon("image/Key_Image/Left_Move.gif");
 		leftMoveLabel = new JLabel(leftMoveIcon);
-		leftMoveLabel.setBounds(20, 65, 40, 40);
+		leftMoveLabel.setBounds(10, 65, 40, 40);
 
-		frontMoveIcon = new ImageIcon("image/gif/Front_Move.gif");
+		frontMoveIcon = new ImageIcon("image/Key_Image/Front_Move.gif");
 		frontMoveLabel = new JLabel(frontMoveIcon);
-		frontMoveLabel.setBounds(60, 65, 40, 40);
+		frontMoveLabel.setBounds(50, 65, 40, 40);
 
-		rightMoveIcon = new ImageIcon("image/gif/Right_Move.gif");
+		rightMoveIcon = new ImageIcon("image/Key_Image/Right_Move.gif");
 		rightMoveLabel = new JLabel(rightMoveIcon);
-		rightMoveLabel.setBounds(100, 65, 40, 40);
+		rightMoveLabel.setBounds(90, 65, 40, 40);
 
-		keyIcon = new ImageIcon("image/moveKey.png");
+		keyIcon = new ImageIcon("image/Key_Image/wasd.png");
 		keyLabel = new JLabel(keyIcon);
-		keyLabel.setBounds(160, 20, 120, 120);
+		keyLabel.setBounds(150, 20, 120, 120);
+
+//		-----------------------------------------------------------------
+
+		AKeyIcon = new ImageIcon("image/Key_Image/A(36x36).png");
+		AKeyLabel = new JLabel(AKeyIcon);
+		AKeyLabel.setBounds(74, 135, 36, 36);
+
+		JumpIcon = new ImageIcon("image/gif/jump.gif");
+		JumpLabel = new JLabel(JumpIcon);
+		JumpLabel.setBounds(10, 130, 40, 40);
+
+		SKeyIcon = new ImageIcon("image/Key_Image/S(36x36).png");
+		SKeyLabel = new JLabel(SKeyIcon);
+		SKeyLabel.setBounds(74, 201, 36, 36);
+
+		FlagIcon = new ImageIcon("image/GameObject/RedFlag.png");
+		FlagLabel = new JLabel(FlagIcon);
+		FlagLabel.setBounds(10, 201, 40, 40);
+
+		QMIcon = new ImageIcon("image/GameObject/Q_Mark.png");
+		QMLable = new JLabel(QMIcon);
+		QMLable.setBounds(134,201,40,40);
+		
+		SKeyIcon2 = new ImageIcon("image/Key_Image/S(36x36).png");
+		SKeyLable2 = new JLabel(SKeyIcon2);
+		SKeyLable2.setBounds(194,201,36,36);
+		
+		SKeyIcon3 = new ImageIcon("image/Key_Image/S(36x36).png");
+		SKeyLable3 = new JLabel(SKeyIcon3);
+		SKeyLable3.setBounds(234,201,36,36);
+		
+		DKeyIcon = new ImageIcon("image/Key_Image/D(36x36).png");
+		DKeyLabel = new JLabel(DKeyIcon);
+		DKeyLabel.setBounds(124, 267, 36, 36);
+
+		ItemIcon = new ImageIcon("image/GameObject/Item/HP_Potion.gif");
+		ItemLabel = new JLabel(ItemIcon);
+		ItemLabel.setBounds(10, 267, 40, 40);
+
+		ItemIcon2 = new ImageIcon("image/GameObject/Item/Radar.gif");
+		ItemLabel2 = new JLabel(ItemIcon2);
+		ItemLabel2.setBounds(64, 267, 40, 40);
 
 		mainPanel.add(backMoveLabel);
 		mainPanel.add(leftMoveLabel);
 		mainPanel.add(frontMoveLabel);
 		mainPanel.add(rightMoveLabel);
 		mainPanel.add(keyLabel);
+		mainPanel.add(AKeyLabel);
+		mainPanel.add(SKeyLabel);
+		mainPanel.add(DKeyLabel);
+		mainPanel.add(JumpLabel);
+		mainPanel.add(FlagLabel);
+		mainPanel.add(QMLable);
+		mainPanel.add(SKeyLable2);
+		mainPanel.add(SKeyLable3);
+		mainPanel.add(ItemLabel);
+		mainPanel.add(ItemLabel2);
 		add(backButton);
 		add(panelPane);
 
